050e0bc2e6a19e79391a390e3297670a28368156
Courtney Jones <courtneyrjones95@gmail.com>
2020-06-23 21:41:39 -0500

    Fixed timer issues

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+
+
 var quizEl = document.getElementById("quiz");
 var startEl = document.getElementById("startBtn");
 var startingTextEl = document.getElementById("startingText");
@@ -11,82 +13,112 @@ var endSection = document.getElementById("end-game");
 var correctAnswer = 0;
 var incorrectAnswer = 0;
 var endScore = document.getElementById("end-score");
-endScore = 0;
+
 
 //time variables
 var timeCount = document.getElementById("timer");
-var secondsLeft = 10;
+var secondsLeft = 50;
 
 //create questions
-var myQuestions = ["Commonly used data types DO NOT include:","Which of the following is most commonly used to style a webpage?"];
-var answersA = ["strings", "javascript"];
-var answersB = ["booleans", "CSS"];
-var answersC = ["alerts", "HTML"];
-var answersD = ["numbers", "C++"];
-var correct = ["alerts", "CSS"];
+var myQuestions = ["How many times have the Celtics played the Lakers in the NBA finals?", "Which player was not apart of the BIG 3 treo?", "When was the last Celtics championship won?", "Who was the first African American drafted to the celtics (And the NBA)?", "Which team has won the most NBA championships?"];
+var answersA = ["10", "Paul Pierce","2002", "Chuck Cooper", "Lakers"];
+var answersB = ["8", "Rajan Rondo","1995", "Kyrie Irving", "Nuggets"];
+var answersC = ["15", "Ray Allen","2012", "Jonas Neil", "Celtics"];
+var answersD = ["12", "Kevin Garnet","2008", "Ryan Stephens", "Cavs"];
+var correct = ["12", "Rajan Rondo","2008", "Chuck Cooper", "Celtics"];
 
+
+var playersScore =[];
+var scoreInput = document.getElementById("score-field");
+var saveBtn = document.getElementById("save-score");
+var scoreBoard = document.getElementById("player-score");
+
+var highscoreEl = document.getElementById("highscores");
+var clearBtn = document.getElementById("clear");
+var goBackBtn = document.getElementById("go-back");
+
+saveBtn.addEventListener("click", function(){
+  playersScore.push({initals: scoreInput.value, score:finalScore});
+  
+})
+
+
+
+//questions index
+var i = -1;
 
 //Start the timer and end the timer calls for question function
 function startQuiz() {
+  startingTextEl.style.display = "none";
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeCount.textContent = secondsLeft;
-    startingTextEl.style.display = "none";
+    
 
 
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
+      // stop showing questions
+    questionAnswerEl.style.display = "none";
+    // show score entry
+    endSection.style.display = "block";
       //show form and score
+    
     }
 
   }, 1000);
   questionAnswerEl.style.display = "block";
-  displayQuestion(timerInterval);
- 
+  displayQuestion();
+
 }
 
 //listen for click event
 function handleAnswerClick() {
-  console.log("Event target");
   console.log(event.target.innerHTML);
-
   if (event.target.innerHTML == correct[i]) {
     console.log(correct[i]);
     correctAnswer++;
-      console.log(correctAnswer);
+    console.log(correctAnswer);
+    finalScore = correctAnswer * 10 + "/50";
+    console.log(finalScore);
+    endScore.textContent = finalScore;
 
+  } else if (event.target.innerHTML != correct[i]) {
+    secondsLeft -= 10;
   }
   displayQuestion();
 }
 
-var i = -1;
+
 
 function displayQuestion() {
   i++;
-  // document.getElementById("questions") = "Q. " + myQuestions[i];
+  document.getElementById("questions").innerText = myQuestions[i];
   document.getElementById("a").innerText = answersA[i];
   document.getElementById("b").innerText = answersB[i];
   document.getElementById("c").innerText = answersC[i];
   document.getElementById("d").innerText = answersD[i];
+
   if (i === myQuestions.length) {
+    // stop showing questions
     questionAnswerEl.style.display = "none";
+    // show score entry
     endSection.style.display = "block";
-    // clearInterval(time);
     handleAnswerClick();
+
+    clearInterval(timeCount);
   }
-return i;
+  return i;
 }
 
 
 //add event listeners
 startEl.addEventListener("click", startQuiz);
-questionAnswerEl.addEventListener("click", function(){
+questionAnswerEl.addEventListener("click", function () {
   handleAnswerClick();
 });
 
-// answersEl.addEventListener("click", handleAnswerClick);
-//listen to the question element for clicks,
-// use innerText to see the value of the button (might have to use the children() method)
+
 
 
 
